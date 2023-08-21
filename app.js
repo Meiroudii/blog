5083564babcd3b123bfa7a71f26b6f6edb7b4f22
Meiroudii <119019700+Meiroudii@users.noreply.github.com>
2023-08-21 10:46:57 +0800

    update:

--- app.js
+++ app.js
@@ -2,9 +2,15 @@
 require('dotenv').config();
 
 const express = require('express');
+const expressLayout = require('express-ejs-layout');
 
 const app = express();
 const PORT = 5000 || process.env.PORT;
+
+// Middleware : Templating engine
+app.use(expressLayout);
+app.set('layout', './layout/main');
+app.set('view engine', 'ejs');
 
 app.get('', (req, res) => {
   res.send("Hello word");
